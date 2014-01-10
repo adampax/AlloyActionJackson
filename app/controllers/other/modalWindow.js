7c421b606d19d6b818299c7029a7f5a16a66088d
Adam Paxton <adampax@gmail.com>
2014-01-09 18:02:21 -0600

    remove up affordance from modal window, since I suppose it shouldn't be shown on modals -- AND it crashes the app starting around Ti SDK 3.1.3 or so

--- app/controllers/other/modalWindow.js
+++ app/controllers/other/modalWindow.js
@@ -5,19 +5,3 @@
     
     For more info, see: http://docs.appcelerator.com/titanium/latest/#!/guide/Android_Themes
  */
-
-function doOpen(e){
-	var actionBar = e.source.activity.actionBar;
-
-	//up window
-	//http://docs.appcelerator.com/titanium/latest/#!/guide/Android_Action_Bar-section-36735509_AndroidActionBar-OtherActionBarFeatures
-
-	if (actionBar) {
-		actionBar.displayHomeAsUp = true;
-		actionBar.onHomeIconItemSelected = function() {
-			e.source.close();
-		};
-
-		e.source.activity.invalidateOptionsMenu();
-	}	
-}
