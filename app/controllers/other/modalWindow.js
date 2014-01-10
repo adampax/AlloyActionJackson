13d3fa34277ab47327bb38960a70823d7ddd13b2
Adam Paxton <adampax@gmail.com>
2014-01-09 18:09:23 -0600

    Update to Ti 3.2.0. FIx issue with modal window no longer getting custom theme in 3.2.0.

--- app/controllers/other/modalWindow.js
+++ app/controllers/other/modalWindow.js
@@ -4,4 +4,12 @@
             		android:name="org.appcelerator.titanium.TiModalActivity" />
     
     For more info, see: http://docs.appcelerator.com/titanium/latest/#!/guide/Android_Themes
+    
+    1/8/14
+    NOTE: Starting in Ti 3.2.0, TiModalActivity is no longer being used, however, TiTranslucentActivity is, and 
+    it has a default theme applied to it (and modal windows), so the the above tiapp.xml entry needs to be changed to:
+    
+    <activity android:configChanges="keyboardHidden|orientation|screenSize" 
+            		android:name="org.appcelerator.titanium.TiTranslucentActivity" />
+     
  */
